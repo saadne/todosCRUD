a4c9097251235c3a5f34d43e0ccf69593f806800
saadne <saadne2018@gmail.com>
2023-06-21 12:14:51 +0000

    Fixing some issues

--- src/main/java/com/saadne/firstSpringApp/welcome/WelcomeController.java
+++ src/main/java/com/saadne/firstSpringApp/welcome/WelcomeController.java
@@ -1,21 +1,30 @@
 package com.saadne.firstSpringApp.welcome;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 @Controller
 @SessionAttributes("name")
-public class WelocmeController {
+public class WelcomeController {
 	
 	@RequestMapping(value="/", method= RequestMethod.GET)
 	public String welcomePage( ModelMap model) {
-		model.put("name", "Saadne");
+		String username = getLoggedInUsername(model);
+		model.put("name", username.substring(0, 1).toUpperCase() + username.substring(1));
 		return "welcome";
 	}
+	private String getLoggedInUsername(ModelMap model) {
+		Authentication authentication =
+				SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
+	}
+
+
 	
 }
